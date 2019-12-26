16bbb802e404fa4f073826190225ae96fd2231e2
Sascha Roth <sascha.roth@kobil.com>
2019-12-26 11:33:06 +0100

    fix test

--- chickenTime/src/androidTest/java/org/chickenhook/chickenTime/ExampleInstrumentedTest.kt
+++ chickenTime/src/androidTest/java/org/chickenhook/chickenTime/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.chickenhook.myandroidhackinglibrary.test", appContext.packageName)
+        assertEquals("org.chickenhook.chickentime.test", appContext.packageName)
     }
 }
